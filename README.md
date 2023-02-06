83d4f55c83548bbc6362b4e98ce98aef8f1a9123
Zubin Khavarian <zubin.khavarian@gmail.com>
2023-02-06 10:45:41 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# zkMake: Three.js Example - Roll It
+
+### Resources:
+
+- [React Three Fiber](https://docs.pmnd.rs/react-three-fiber/): Three.js Renderer
+- [Drei](https://github.com/pmndrs/drei): React Three Fiber Helpers
+- [Three.js](https://threejs.org/docs/index.html#manual/en/introduction/Creating-a-scene): 3D Engine
+- [Vite](https://vitejs.dev/guide/): Static Web Server
---
+++ src/components/Lights.tsx
@@ -0,0 +1,27 @@
+import React, { useRef } from "react";
+import { useHelper } from "@react-three/drei";
+import {
+  DirectionalLightHelper,
+  // DirectionalLight
+} from "three";
+
+import { useDebugContext } from "../DebugContext";
+
+export const Lights = () => {
+  const { debug } = useDebugContext();
+  const lightRef = useRef<any>(null); // DirectionalLight
+  useHelper(debug && lightRef, DirectionalLightHelper);
+
+  return (
+    <>
+      <directionalLight
+        ref={lightRef}
+        position={[4, 10, 2]}
+        castShadow={true}
+      />
+      <ambientLight intensity={0.3} />
+    </>
+  );
+};
+
+export default Lights;
